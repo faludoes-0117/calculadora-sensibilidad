a05d9034f31ce0c4d73ec079b6eb779d58b7515a
faludoes-0117 <falu.does@gmail.com>
2026-03-02 18:37:46 -0300

    Implementacion de sitemap y robots.txt para SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://faludoes-0117.github.io',
   base: '/calculadora-sensibilidad',
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
 });
